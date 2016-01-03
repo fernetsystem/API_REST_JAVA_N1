6bdd21e1ec9042ce16a2e6860b03bef482fe05a6
Jth Rdz <fernetsystem@gmail.com>
2016-01-02 19:28:24 -0600

    agregar librerias para convertir la clase en servicio
    Apareceran warning en la linea ocho dar clic en la primera opcion

--- src/java/com/elity/services.java
+++ src/java/com/elity/services.java
@@ -1,6 +1,10 @@
 
 package com.elity;
+import javax.ejb.Stateless;
+import javax.ws.rs.Path;
 
+@Stateless              //esto sirve para convertir a un EJB
+@Path("/services")      //ruta que se consultara los servicios
 public class services {
     
 }
